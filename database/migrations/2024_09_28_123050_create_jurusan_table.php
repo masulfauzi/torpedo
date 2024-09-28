a6fb278f9591ebe204d945bc78b12c36394348d4
IlhamDwi22 <ilhamdwiganteng22@gmail.com>
2024-09-28 19:45:03 +0700

    feat. add table NamaJurusan

---
+++ database/migrations/2024_09_28_123050_create_jurusan_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nama_jurusan', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->string('nama_jurusan');
+
+            $table->timestamps();
+            $table->softDeletes();
+            $table->uuid('created_by')->nullable();
+            $table->uuid('updated_by')->nullable();
+            $table->uuid('deleted_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nama_jurusan');
+    }
+};
